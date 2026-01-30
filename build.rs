11f4e3af5f2569bd3daa5fece13a83fc468c30b8
Promethyxx <peter.mboyo@ik.me>
2026-01-30 17:06:32 +0100

    version number and tools licences

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
 fn main() {
-    // On ajoute NONE comme deuxième argument pour les macros
-    embed_resource::compile("oxyon_icon.rc", embed_resource::NONE);
+    if std::env::var("CARGO_CFG_WINDOWS").is_ok() {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("oxyon_icon.ico"); // Assurez-vous du nom du fichier
+        // Récupère automatiquement la version du Cargo.toml
+        res.set_language(0x040c); 
+        res.compile().unwrap();
+    }
 }
